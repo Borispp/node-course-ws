f1c92e5e9f06a659062d76ce68faebf2b729a82f
borisvorob <borisss777@gmail.com>
2017-09-12 10:39:23 +0300

    Initial

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+// const hbs = require('hbs');
+
+var app = express();
+
+// app.set('view engine', 'hbs');
+// http://localhost:3004/help.html
+app.use(express.static(__dirname + '/public'));
+
+// My own middleware
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  console.log(`${now}: ${req.method} ${req.url}`);
+  next();
+})
+
+app.get('/', function (req, res) {
+  res.send({name: "Boris"});
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: "Page title",
+    currentYear: new Date().getFullYear()
+  });
+});
+
+app.listen(3004);
